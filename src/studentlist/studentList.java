bf8e048f5c268ffd0e5c083c4bbdfb0ec2b76c5c
Harmandeep Singh Aulakh <aulakh8@sheridancollege.ca>
2022-02-10 15:00:44 -0500

    new parameter added

--- src/studentlist/studentList.java
+++ src/studentlist/studentList.java
@@ -12,9 +12,9 @@ package studentlist;
 public class studentList {
     public static void main(String[] args){
         Student[] studentList = new Student[3];
-        studentList[0] = new Student("1","Ronak Sheth");
-        studentList[1] = new Student("2","John Doe");
-        studentList[2] = new Student("3","abc");
+        studentList[0] = new Student("1","Ronak Sheth","20");
+        studentList[1] = new Student("2","John Doe","30");
+        studentList[2] = new Student("3","abc","40");
         
         for(int  i = 0;i<studentList.length;i++){
             System.out.println(studentList[i].getStudentName());
